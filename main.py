98aa54b71462d7e20768ffa982ac1feda0134f49
George Jordan <142242414+DoubleGlos@users.noreply.github.com>
2026-02-13 10:19:32 +0000

    created count characters function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,6 +8,8 @@ def main():
     path_to_book = "books/frankenstein.txt"
     book_text = get_book_text(path_to_book)
     num_words = count_words(book_text)
+    num_characters = count_characters(book_text)
     print(f"Found {num_words} total words")
+    print(f"{num_characters}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,11 @@
 def count_words(text):
     return len(text.split())
+
+def count_characters(characters):
+    sorted_characters = {}
+    characters_to_count = list(characters.lower())
+    for character in characters_to_count:
+        if character not in sorted_characters:
+            sorted_characters[character] = 0
+        sorted_characters[character] += 1
+    return sorted_characters
