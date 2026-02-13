6b7497ea0b64c66ac43895a3f0d5e5ce89d189f3
George Jordan <142242414+DoubleGlos@users.noreply.github.com>
2026-02-13 08:04:45 +0000

    added get book text function

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+    
+def main():
+    path_to_book = "books/frankenstein.txt"
+    print(get_book_text(path_to_book))
+
+main()
