a51d72443de5b5775be564dfbfa040f4d40c475f
George Jordan <142242414+DoubleGlos@users.noreply.github.com>
2026-02-19 14:27:02 +0000

    Finished project by completing the create_report function. Also added functionality to allow command line input using sys.argv

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from stats import count_words, count_characters
+import sys
+from stats import count_words, count_characters, create_report
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
   
 def main():
-    path_to_book = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    path_to_book = sys.argv[1]
     book_text = get_book_text(path_to_book)
     num_words = count_words(book_text)
     num_characters = count_characters(book_text)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_book}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(f"{num_characters}")
+    print("--------- Character Count -------")
+    sorted_chars = create_report(num_characters)
+    for entry in sorted_chars:
+        if entry["char"].isalpha():
+            print(f"{entry['char']}: {entry['num']}")
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,14 @@ def count_characters(characters):
             sorted_characters[character] = 0
         sorted_characters[character] += 1
     return sorted_characters
+
+def create_report(sorted_characters):
+    report = []
+    for char, count in sorted_characters.items():
+        report_entry = {"char":char , "num":count}
+        report.append(report_entry)
+    report.sort(reverse=True, key=sort_on)
+    return report
+
+def sort_on(items):
+    return items["num"]
